2a57f6a1c2adb914b3269ac462385e8eab4d4160
ibei <catcher2333@163.com>
2018-11-02 19:45:27 +0900

    motoman

--- moveit_moto_2/src/with_rviz/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tjosh/sia10f_motoman_ws/src/with_rviz
-# Build directory: /home/tjosh/sia10f_motoman_ws/src/with_rviz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
